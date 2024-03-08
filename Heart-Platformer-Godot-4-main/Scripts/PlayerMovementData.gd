ed04eaa8613cc92bfc6fe9fd6f1f7bf254947af2
Lesosa12 <Lesosa@s.sfusd.edu>
2024-03-08 14:32:55 -0800

    work of 3-8

--- Heart-Platformer-Godot-4-main/Scripts/PlayerMovementData.gd
+++ Heart-Platformer-Godot-4-main/Scripts/PlayerMovementData.gd
@@ -2,9 +2,9 @@ class_name PlayerMovementData
 extends Resource
 
 @export var speed = 100.0
-@export var acceleration = 800.0
-@export var friction = 1000.0
+@export var acceleration = 1000
+@export var friction = 1000
 @export var jump_velocity = -300.0
 @export var gravity_scale = 1.0
 @export var air_resistance = 200.0
-@export var air_acceleration = 400.0
+@export var air_acceleration = 800.0
--- Heart-Platformer-Godot-4-main/Scripts/door.gd
+++ Heart-Platformer-Godot-4-main/Scripts/door.gd
@@ -1,15 +1,31 @@
 extends Area2D
 
 @export var scene_to_load: String
-@export var spawn_point_path: NodePath
+
 
 func _on_body_entered(body):
 	print("Player collided with the door")
 	$CollisionShape2D.set_deferred("disabled", true)
 	load_scene(scene_to_load)
+	
 
 func load_scene(path):
 	if path != "" and path != null:
 		var scene_instance = ResourceLoader.load(path)
 		if scene_instance:
 			get_tree().change_scene_to_packed(scene_instance)
+
+# In the previous scene (when player interacts with the door)
+func _on_door_interaction():
+	var nextScene = "res://next_scene.tscn"
+	var spawnPoint = "spawn_point_name"
+	Global.nextSceneSpawnPoint = spawnPoint
+	get_tree().change_scene(nextScene)
+
+# In the new scene (when the scene is loaded)
+func _on_scene_loaded():
+	if Global.nextSceneSpawnPoint:
+		var playerSpawnPoint = get_node(Global.nextSceneSpawnPoint)
+		player.position = playerSpawnPoint.global_position
+
+
