f25b3c00c9265c2e56654852af38e062200e634f
Lesosa12 <Lesosa@s.sfusd.edu>
2024-03-14 14:31:32 -0700

    work of 3-14
    yippee pi day

--- Heart-Platformer-Godot-4-main/Scripts/door.gd
+++ Heart-Platformer-Godot-4-main/Scripts/door.gd
@@ -3,16 +3,35 @@ extends Area2D
 # Export the next scene path
 @export var nextScene: String = "" #fill with the scene path like: res://Scenes/level_two.tscn
 # Export the door name
-@export var doorName: String = "" #name door and spawn point something unique to link
+@export var connectedSpawnPoint: String = ""
 
 func _ready():
 	pass
 
-func _on_body_entered(body):
-	print("Body Entered:", body.name)
-	if body.name == "Player":
-		change_scene()
-
 func change_scene():
 	var nextSceneInstance = load(nextScene)
-	get_tree().change_scene_to_packed(nextSceneInstance)
+	if nextSceneInstance == null:
+		print("Error loading scene:", nextScene)
+		return
+
+	var nextSceneNode = nextSceneInstance.instance()
+	if nextSceneNode == null:
+		print("Error instancing scene:", nextScene)
+		return
+
+	# Find the connected spawn point in the next scene
+	var spawnPointPath = "SpawnPoints/" + spawnPointName
+	var spawnPoint = nextSceneNode.get_node(spawnPointPath)
+	if spawnPoint == null:
+		print("Error: Spawn point not found for door:", spawnPointName)
+		return
+
+	# Set the player's spawn point in the next scene
+	var playerNode = nextSceneNode.get_node("PlayerNodeName")
+	if playerNode != null:
+		playerNode.global_position = spawnPoint.global_position
+	else:
+		print("Error: Player node not found in the scene")
+
+	# Change the scene
+	get_tree().change_scene_to_packed(nextSceneNode)
