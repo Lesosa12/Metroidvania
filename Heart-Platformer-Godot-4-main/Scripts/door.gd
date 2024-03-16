9fdfb852ca2990a8148eb08e1a68e25847ec2c25
Lesosa12 <Lesosa@s.sfusd.edu>
2024-03-15 20:29:28 -0700

    doors have unique spawn

--- Heart-Platformer-Godot-4-main/Scripts/door.gd
+++ Heart-Platformer-Godot-4-main/Scripts/door.gd
@@ -18,28 +18,6 @@ func change_scene():
 		print("Error loading scene:", nextScene)
 		return
 
-	var nextSceneNode = nextSceneInstance
-	if nextSceneNode == null:
-		print("Error instancing scene:", nextScene)
-		return
-
-	# Debug print to check the type of nextSceneNode
-	print("Type of nextSceneNode:", nextSceneNode.get_type_name())
-
-	# Find the connected spawn point in the next scene
-	var spawnPointPath = "SpawnPoints/" + spawnPointName
-	print("Spawn Point Path:", spawnPointPath)
-	var spawnPoint = nextSceneNode.get_node(spawnPointPath)
-	if spawnPoint == null:
-		print("Error: Spawn point not found for door:", spawnPointName)
-		return
-
-	# Set the player's spawn point in the next scene
-	var playerNode = nextSceneNode.get_node("PlayerNodeName")
-	if playerNode != null:
-		playerNode.global_position = spawnPoint.global_position
-	else:
-		print("Error: Player node not found in the scene")
-
 	# Change the scene
-	get_tree().change_scene_to_packed(nextSceneNode)
+	get_tree().paused = false
+	get_tree().change_scene_to_packed(nextSceneInstance)
