828de49ebf362f9ab844190b8dbb5d30df81c147
Lesosa12 <Lesosa@s.sfusd.edu>
2024-04-03 20:53:30 -0700

    work of 4-3

--- Heart-Platformer-Godot-4-main/Scripts/Patrolling Enemy.gd
+++ Heart-Platformer-Godot-4-main/Scripts/Patrolling Enemy.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
+var enemy_death_effect = preload("res://Scenes/enemy_death_effect.tscn")
 @export var patrol_points : Node
 @export var speed : int = 1500
 @export var wait_time : int = 3
+@export var health_amount : int = 3
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var timer = $Timer
@@ -17,6 +19,7 @@ var point_positions : Array[Vector2]
 var current_point : Vector2
 var current_point_position : int
 var can_walk : bool
+
 
 func _ready():
 	if patrol_points != null:
@@ -81,3 +84,17 @@ func enemy_animations():
 
 func _on_timer_timeout():
 	can_walk = true
+
+
+func _on_hurtbox_area_entered(area):
+	print("Hurtbox area entered")
+	if area.get_parent().has_method("get_damage_amount"):
+		var node =  area.get_parent() as Node
+		health_amount -= node.damage_amount
+		print("Health amount: ", health_amount)
+		
+		if health_amount <= 0:
+			var enemy_death_effect_intance = enemy_death_effect.instantiate() as Node2D
+			enemy_death_effect_intance.global_position = global_position
+			get_parent().add_child(enemy_death_effect_intance)
+			queue_free()
--- Heart-Platformer-Godot-4-main/Scripts/bullet.gd
+++ Heart-Platformer-Godot-4-main/Scripts/bullet.gd
@@ -1,5 +1,7 @@
 extends AnimatedSprite2D
 
+var bullet_impact_effect = preload("res://Scenes/bullet_impact_effect.tscn")
+
 var speed  : int = 600
 var direction : int
 
@@ -12,11 +14,19 @@ func _physics_process(delta):
 func _on_timer_timeout():
 	queue_free()
 
-
 func _on_hitbox_area_entered(area):
 	print("Bullet area entered")
-
-
+	bullet_impact()
 
 func _on_hitbox_body_entered(body):
 	print("Bullet body entered")
+	bullet_impact()
+
+func get_damage_amount():
+	pass
+
+func bullet_impact():
+	var bullet_impact_effect_instance = bullet_impact_effect.instantiate() as Node2D
+	bullet_impact_effect_instance.global_position = global_position
+	get_parent().add_child(bullet_impact_effect_instance)
+	queue_free()
